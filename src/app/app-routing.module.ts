4fa71d24be93fc14507b0f85d70b19c567556b7c
raressarachie <52413169+raressarachie@users.noreply.github.com>
2019-07-25 14:51:09 +0300

    Revert "Rares/fix headers"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UsersComponent } from './users/users.component';
-import { CreateUserComponent } from './users/create-user/create-user.component';
 
-const routes: Routes = [
-  { path: 'users/new', component: CreateUserComponent},
-  { path: 'users', component: UsersComponent},
 
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
