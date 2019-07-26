dd5e7b5b5b24e44cec9ba9bd3457e5df1a15c702
Denise Orosz <denise.orosz@gmail.com>
2019-07-26 10:03:01 +0300

    functional version

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -17,6 +17,13 @@ export class UserService {
     return Math.max.apply(Math, USERS.map(o => o.id)) + 1;
   }
 
+  getUserById(id:number):User{
+    for(let i= 0; i< USERS.length; i++){
+      if(USERS[i].id===id)
+       return USERS[i];
+    }
+  }
+
   saveUser(user) {
     for (let i =0; i< USERS.length; i++)
     {
